a829eeed6bd6c5b8b9b889100e8e74bd50c3b0b4
1633261194-source <1633261194@qq.com>
2026-09-03 18:02:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lingyu-ledger-v8';
+const CACHE_NAME = 'lingyu-ledger-v9';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './assets/character-avatar.png', './assets/character-main.png'];
 
 self.addEventListener('install', (event) => {
